b171add9e657b2ec5ef7cfc0d6042cca13aa94d6
Jim <neo125874@gmail.com>
2015-07-08 17:55:09 +0800

    fb login.

--- app/src/main/java/enfamama/mjn/telexpress/com/loginwithfb/MainActivity.java
+++ app/src/main/java/enfamama/mjn/telexpress/com/loginwithfb/MainActivity.java
@@ -1,19 +1,138 @@
 package enfamama.mjn.telexpress.com.loginwithfb;
 
+import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.Signature;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Base64;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.Toast;
 
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+import com.facebook.Profile;
+import com.facebook.ProfileTracker;
 import com.facebook.appevents.AppEventsLogger;
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.squareup.picasso.Picasso;
+
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 
 
 public class MainActivity extends ActionBarActivity {
 
+    private CallbackManager callbackManager;
+    private LoginButton fbLoginButton;
+    private Profile mProfile;
+    private ImageView fbImageView;
+    private ProfileTracker profileTracker;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
+
+        //You need this method to be used only once to configure
+        //your key hash in your App Console at
+        // developers.facebook.com/apps
+        getFbKeyHash("enfamama.mjn.telexpress.com.loginwithfb");
+
         setContentView(R.layout.activity_main);
+
+        fbLoginButton = (LoginButton)findViewById(R.id.fb_login_button);
+        fbLoginButton.setReadPermissions("user_posts");
+
+        fbLoginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+
+
+
+                System.out.println("Facebook Login Successful!");
+                System.out.println("Logged in user Details : ");
+                System.out.println("--------------------------");
+                System.out.println("User ID  : " + loginResult.getAccessToken().getUserId());
+                System.out.println("Authentication Token : " + loginResult.getAccessToken().getToken());
+                Toast.makeText(MainActivity.this, "Login Successful!", Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void onCancel() {
+                Toast.makeText(MainActivity.this, "Login cancelled by user!", Toast.LENGTH_LONG).show();
+                System.out.println("Facebook Login failed!!");
+
+            }
+
+            @Override
+            public void onError(FacebookException e) {
+                Toast.makeText(MainActivity.this, "Login unsuccessful!", Toast.LENGTH_LONG).show();
+                System.out.println("Facebook Login failed!!");
+            }
+        });
+
+        profileTracker = new ProfileTracker() {
+            @Override
+            protected void onCurrentProfileChanged(
+                    Profile oldProfile,
+                    Profile currentProfile) {
+
+                mProfile = currentProfile;
+                if(mProfile!=null)
+                    Picasso.with(MainActivity.this)
+                            .load(mProfile.getProfilePictureUri(200, 200))
+                            .into(fbImageView);
+                else
+                    Picasso.with(MainActivity.this)
+                    .load("http://www.aheritier.net/wp-content/plugins/all-in-one-seo-pack/images/default-user-image.png")
+                    .resize(200, 200)
+                    .into(fbImageView);
+            }
+        };
+        mProfile = Profile.getCurrentProfile();
+        fbImageView = (ImageView)findViewById(R.id.fb_profile_imageView);
+
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        profileTracker.stopTracking();
+    }
+
+    @Override
+    protected void onActivityResult(int reqCode, int resCode, Intent i) {
+        callbackManager.onActivityResult(reqCode, resCode, i);
+    }
+
+    public void getFbKeyHash(String packageName) {
+
+        try {
+            PackageInfo info = getPackageManager().getPackageInfo(
+                    packageName,
+                    PackageManager.GET_SIGNATURES);
+            for (Signature signature : info.signatures) {
+                MessageDigest md = MessageDigest.getInstance("SHA");
+                md.update(signature.toByteArray());
+                Log.d("YourKeyHash :", Base64.encodeToString(md.digest(), Base64.DEFAULT));
+                System.out.println("YourKeyHash: "+ Base64.encodeToString(md.digest(), Base64.DEFAULT));
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+
+        } catch (NoSuchAlgorithmException e) {
+
+        }
+
     }
 
     @Override
@@ -21,7 +140,7 @@ public class MainActivity extends ActionBarActivity {
         super.onResume();
 
         // Logs 'install' and 'app activate' App Events.
-        AppEventsLogger.activateApp(this);
+        AppEventsLogger.activateApp(this, getString(R.string.facebook_app_id));
     }
 
     @Override
@@ -29,7 +148,7 @@ public class MainActivity extends ActionBarActivity {
         super.onPause();
 
         // Logs 'app deactivate' App Event.
-        AppEventsLogger.deactivateApp(this);
+        AppEventsLogger.deactivateApp(this, getString(R.string.facebook_app_id));
     }
 
     @Override
